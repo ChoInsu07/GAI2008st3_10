2d82923de843e3d8f677fecd1b0de9072ec04b24
yujianuo <yujianuo86@gmail.com>
2025-05-08 11:55:06 +0900

    add def function

--- GAI2008st3_10.py
+++ GAI2008st3_10.py
@@ -9,3 +9,29 @@ def get_shift_amount():
 
 def remove_nonletters(input_text):
     return 'asdf'
+
+def get_shift_amount():
+    # 使用 argparse 获取 shift 参数
+    return 1
+
+def remove_nonletters(input_text):
+    # 去除非字母，只保留字母
+    return 'asdf'
+
+def cipher(text, shift_amount):
+    # 加密，每5个字母插入空格
+    return 'zxcv'
+
+def decipher(text, shift_amount):
+    # 解密，把空格去掉并做逆向位移
+    return 'asdf'
+
+
+if __name__ == '__main__':
+    original_text = get_original_text()
+    shift_amount = get_shift_amount()
+    text_letter_only = remove_nonletters(original_text)
+    cipher_text = cipher(text_letter_only, shift_amount)
+    print(f'cipher_text=> {cipher_text}')
+    decipher_text = decipher(cipher_text, shift_amount)
+    print(f'decipher_text=> {decipher_text}')

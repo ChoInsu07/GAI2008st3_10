30563e93e0206d6598368bf8113dadefc0f565d2
ChoInsu07 <iwantsleep1007@yonsei.ac.kr>
2025-05-08 11:45:48 +0900

    add def function

--- GAI2008st3_10.py
+++ GAI2008st3_10.py
@@ -1,2 +1,11 @@
 #2024106140 choinsu
 #2023106117 yujianuo
+
+def get_originalItext():
+    return 'as df'
+
+def get_shift_amount():
+    return 1
+
+def remove_nonletters(input_text):
+    return 'asdf'
